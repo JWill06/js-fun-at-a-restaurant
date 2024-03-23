fc9773ea103076eb6e774aee57546bc5cd2b00c4
jordan williamson <williamsonjordan05@gmail.com>
2024-03-23 12:22:43 -0600

    Updated final commit for restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -2,48 +2,42 @@ function createRestaurant(name, menus){
   var pizzaRestaurant = {
     name: name,
     menus: {
-      lunch: [],
-      breakfast: [],
-      dinner: []
+      lunch: [], // [item.type === lunch]
+      breakfast: [],// [item.type === breakfast]
+      dinner: [] //[item.type === dinner]
     }
   }
   return pizzaRestaurant
 }
 
 function addMenuItem(restaurant, item){
-var type = item.type;
-var exists = restaurant.menus[type].some(function(existingItem){
-  return existingItem.name === item.name 
-})
-  if(!exists){
-    restaurant.menus[type].push(item);
-  } else {
-  }
-  return restaurant.menus
+ if(restaurant.menus[item.type].includes(item)){
+   return;
+} else {
+restaurant.menus[item.type].push(item)
+   }
 }
 
 function removeMenuItem(restaurant, itemName, menuType) {
-
-  if (!restaurant.menus || !restaurant.menus[menuType]) {
-     return;
+   // Find the index of the item to remove
+   var itemIndex = restaurant.menus[menuType].findIndex(function(menuItem) {
+      return menuItem.name === itemName;
+   });
+  
+   // If the item is found, remove it from the menu
+   if (itemIndex !== -1) {
+      restaurant.menus[menuType].splice(itemIndex, 1);
+      return `No one is eating our ${itemName} - it has been removed from the ${menuType} menu!`;
+   } else {
+      // If the item is not found, return a message indicating that the item is not on the menu
+      return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+   }
   }
- 
-  var itemIndex = restaurant.menus[menuType].findIndex(function(menuItem) {
-     return menuItem.name === itemName;
-  });
- 
-  // If the item is found, remove it from the menu
-  if (itemIndex !== -1) {
-     restaurant.menus[menuType].splice(itemIndex, 1);
-     return `No one is eating our ${itemName} - it has been removed from the ${menuType} menu!`;
-  } else {
-     return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
-  }
- }
+  
  
  function checkForFood(restaurant, item) {
-  console.log("REST:", restaurant);
-  console.log("FOOD:", item);
+//   console.log("REST:", restaurant);
+//   console.log("FOOD:", item);
  
   var menuType = item.type; // This should be 'lunch', 'breakfast', or 'dinner'
  
